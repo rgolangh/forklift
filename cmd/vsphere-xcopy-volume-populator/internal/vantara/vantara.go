e7d1c18079700345a30e1c95111e439728d3dfb1
tatsumir <ryosuke.tatsumi@hal.hitachi.com>
2025-04-03 07:21:56 +0000

    fix hostids

--- cmd/vsphere-xcopy-volume-populator/internal/vantara/vantara.go
+++ cmd/vsphere-xcopy-volume-populator/internal/vantara/vantara.go
@@ -67,7 +67,6 @@ func getStorageEnvVars() (map[string]interface{}, error) {
 }
 
 func getNewVantaraStorageAPIfromEnv(envVars map[string]interface{}, vantaraObj VantaraObject) *VantaraStorageAPI {
-	vantaraObj["hostGroupIds"] = envVars["hostGroupIds"].([]string)
 	return NewVantaraStorageAPI(envVars["storageId"].(string), envVars["restServerIP"].(string), envVars["port"].(string), envVars["userID"].(string), envVars["password"].(string), vantaraObj)
 }
 
